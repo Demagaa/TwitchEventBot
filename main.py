6ee261c7fca9285288ec54978274c9869730d28d
achursin <achursin@mit-consulting.cz>
2024-01-12 13:13:53 +0100

    twitchio integrated

--- main.py
+++ main.py
@@ -6,84 +6,47 @@ import json
 import twitchio
 from flask import Flask, request, jsonify
 import secrets
+import twitchio
 
-# bot = telebot.TeleBot('6667630731:AAFRdAj3DcdvL5LPECZIQzAqKrov6EhT9Nw')
-#
-#
-# @bot.message_handler(commands=['start', 'hello'])
-# def send_welcome(message):
-#     bot.reply_to(message, "Howdy, how are you doing?")
-#
+from twitchio.ext import commands, eventsub
 
-# bot.infinity_polling()
+esbot = commands.Bot.from_client_credentials(client_id='...',
+                                             client_secret='...')
+esclient = eventsub.EventSubClient(esbot,
+                                   webhook_secret='...',
+                                   callback_route='https://your-url.here/callback')
+stream_id = 1
 
-app = Flask(__name__)
+broadcaster = {'broadcaster_user_id' : 106166322,
+               'broadcaster_user_name' : 'Stubborn51',
+               'id' : stream_id,
+               'type' : '1',
+               'started_at' : ''}
+class Bot(commands.Bot):
 
-# Notification request headers
-TWITCH_MESSAGE_ID = 'Twitch-Eventsub-Message-Id'.lower()
-TWITCH_MESSAGE_TIMESTAMP = 'Twitch-Eventsub-Message-Timestamp'.lower()
-TWITCH_MESSAGE_SIGNATURE = 'Twitch-Eventsub-Message-Signature'.lower()
-MESSAGE_TYPE = 'Twitch-Eventsub-Message-Type'.lower()
+    def __init__(self):
+        super().__init__(token='...', prefix='!', initial_channels=['channel'])
 
-# Notification message types
-MESSAGE_TYPE_VERIFICATION = 'webhook_callback_verification'
-MESSAGE_TYPE_NOTIFICATION = 'notification'
-MESSAGE_TYPE_REVOCATION = 'revocation'
+    async def __ainit__(self) -> None:
+        self.loop.create_task(esclient.listen(port=4000))
 
-# Prepend this string to the HMAC that's created from the message
-HMAC_PREFIX = 'sha256='
+        try:
+            await esclient.subscribe_channel_stream_start(broadcaster=106166322)
+        except twitchio.HTTPException:
+            pass
 
-def get_secret():
-    # TODO: Get secret from secure storage. This is the secret you pass
-    # when you subscribed to the event.
-    return ''
-
-# Build the message used to get the HMAC.
-def get_hmac_message(request):
-    return (request.headers[TWITCH_MESSAGE_ID] +
-            request.headers[TWITCH_MESSAGE_TIMESTAMP] +
-            request.data.decode('utf-8'))
-
-# Get the HMAC.
-def get_hmac(secret, message):
-    return HMAC_PREFIX + hmac.new(secret.encode('utf-8'), message.encode('utf-8'), hashlib.sha256).hexdigest()
-
-# Verify whether our hash matches the hash that Twitch passed in the header.
-def verify_message(hmac, verify_signature):
-    return hmac == verify_signature
+    async def event_ready(self):
+        print('Bot is ready!')
 
 
-@app.route('/streamstart', methods=['POST'])
-def streamstart():
-    secret = get_secret()
-    message = get_hmac_message(request)
-    hmac_value = get_hmac(secret, message)
+bot = Bot()
+bot.loop.run_until_complete(bot.__ainit__())
 
-    if verify_message(hmac_value, request.headers[TWITCH_MESSAGE_SIGNATURE]):
-        print("Signatures match")
 
-        # Get JSON object from body, so you can process the message.
-        notification = json.loads(request.data.decode('utf-8'))
+@esbot.event()
+async def event_notification_stream_start(payload: eventsub.StreamOnlineData) -> None:
+    print('Received event!')
+    channel = bot.get_channel('channel')
+    await channel.send(f'{payload.data.user.name} started stream woohoo!')
 
-        if MESSAGE_TYPE_NOTIFICATION == request.headers[MESSAGE_TYPE]:
-            # TODO: Do something with the event's data.
-            print(f"Event type: {notification['subscription']['type']}")
-            print(json.dumps(notification['event'], indent=4))
-            return '', 204
-        elif MESSAGE_TYPE_VERIFICATION == request.headers[MESSAGE_TYPE]:
-            return notification['challenge'], 200, {'Content-Type': 'text/plain'}
-        elif MESSAGE_TYPE_REVOCATION == request.headers[MESSAGE_TYPE]:
-            print(f"{notification['subscription']['type']} notifications revoked!")
-            print(f"Reason: {notification['subscription']['status']}")
-            print(f"Condition: {json.dumps(notification['subscription']['condition'], indent=4)}")
-            return '', 204
-        else:
-            print(f"Unknown message type: {request.headers[MESSAGE_TYPE]}")
-            return '', 204
-    else:
-        print('403')  # Signatures didn't match.
-        return '', 403
-
-if __name__ == '__main__':
-    app.run(port=443)
-
+bot.run()
